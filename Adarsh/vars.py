11e51208bf68a834b956514537462fa3b921a6de
Bdgdirect1k <125243759+Bdgdirect1k@users.noreply.github.com>
2023-02-14 09:58:42 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -36,5 +36,5 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://BdgDircts:844013250hack@cluster0.mp7nuzs.mongodb.net/?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', '1166733506'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'netfixseriesbackup'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
